d71be11036c2dc21d4d6d420fd2f710869a1adf7
Yusuf Bolden <ajb2316@columbia.edu>
2025-05-07 01:35:12 -0400

    Created model for game score

--- game/models.py
+++ game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class GameScore(models.model):
+    user_name = models.CharField(max_length=100)
+    score = models.IntegerField()
+    date_played = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_name} - {self.score}"
